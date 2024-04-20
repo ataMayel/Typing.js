fa47c609ecd856c3d87eb8fdb1d459bed27658cf
ataMayel <ataboter@gmail.com>
2024-04-20 18:11:35 +0330

    Publish website.

---
+++ docs/script.js
@@ -0,0 +1,33 @@
+const title = new Typing('#title', {
+    data: 'Typing.js',
+    speed: 100,
+    delay: 1000,
+    repeat: true,
+    back: {
+        speed: 60,
+        delay: 10000
+    }
+})
+
+title.start()
+
+const cdn = document.querySelector('#cdn')
+cdn.innerText = '<script src="https://cdn.jsdelivr.net/gh/ataMayel/Typing.js/typing.min.js"></script>';
+
+const tag = document.querySelector('#tag')
+tag.innerText = '<h1 id="element"></h1>';
+
+const create = document.querySelector('#create')
+create.innerText = `const title = new Typing('#element', {
+    data: 'Hello world!',
+    speed: 60,
+    delay: 1000,
+    repeat: true,
+    back: {
+        speed: 20,
+        delay: 3000
+    },
+})`
+
+const start = document.querySelector('#start')
+start.innerText = 'title.start()'
